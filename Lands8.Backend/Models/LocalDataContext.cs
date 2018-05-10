04b2b201f00d11307fbf82a9bc68ed6242e03deb
crspalacios <cpalacios@crealodigital.com>
2018-05-10 00:20:28 -0500

    Nuevos Usuarios Backend

---
+++ Lands8.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lands8.Backend.Models
+{
+    using Domain;
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Lands8.Domain.User> Users { get; set; }
+    }
+}
